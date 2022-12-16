4e3de28872a6a50163f4deb8ab8492209c7c8801
User <kyle_albright@hotmail.com>
2022-12-16 13:51:36 -0600

    Created some of our files, and started filling in the models

---
+++ models/user.js
@@ -0,0 +1,66 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require(" ../config/connection");
+
+const bcrypt = require("bcrypt");
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4],
+      },
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validatePassword: function (password) {
+        if (
+          !/^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{10,}$/.test(
+            password
+          )
+        ) {
+          throw new Error(
+            "The password must contain at least 10 characters, at least one uppercase letter, one lowercase letter, one number and one special character"
+          );
+        }
+      },
+    },
+  },
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
