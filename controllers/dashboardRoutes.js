69e5e13ea94e9c10b60445b72e7f72f3c24c0de9
User <kyle_albright@hotmail.com>
2022-12-20 17:57:28 -0600

    dashboard routes working. silly errors

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,24 +2,27 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 
 const { Post, User } = require("../models");
-const withAuth = require("../utils/auth");
+const withAuth = require("../utils/auth.js");
 
 //get all posts and show the user who posted
 router.get("/", withAuth, async (req, res) => {
   try {
     const postData = await Post.findAll({
-      where: { userId: req.session.userId },
+      where: { user_id: req.session.userId },
       include: [User],
     });
     // serialization to remove excess data that wont be displayed
     const userPosts = postData.map((post) => post.get({ plain: true }));
-    res.render("posts", {
-      layout: "dashboard",
-      userPosts,
+    console.log(userPosts)
+    res.render("dashboard", {
+      // layout: "dashboard",
+     posts:  userPosts, loggedIn: req.session.loggedIn
     });
   } catch (err) {
+    console.log(err)
     res.redirect("login");
   }
+  // res.render("dashboard")
 });
 // create new post route
 router.get("/create", withAuth, (req, res) => {
